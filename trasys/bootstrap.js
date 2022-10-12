df85f57f1200d7946d716bb6a73c5d0e2aef00aa
Ítalo Rodrigo <italo.computation@gmail.com>
2022-10-12 17:33:14 -0400

    add
    add

--- trasys/bootstrap.js
+++ trasys/bootstrap.js
@@ -1,5 +1,5 @@
 // import bootstrap from '../trasys/node_modules/bootstrap/dist/css/bootstrap.css';
 
-import bootstrap from '@node_modules/bootstrap/dist/css/bootstrap.css';
+import bootstrap from 'bootstrap/dist/js/bootstrap.js';
 
 export default bootstrap;
