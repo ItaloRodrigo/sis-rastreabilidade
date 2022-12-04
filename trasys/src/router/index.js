279800908a00e6ba16d9fe9602f518bda0d7e704
Ítalo Rodrigo <italo.computation@gmail.com>
2022-12-03 20:18:44 -0400

    Lista de Usuários no Front
    Lista de Usuários no Front

--- trasys/src/router/index.js
+++ trasys/src/router/index.js
@@ -5,6 +5,7 @@ import auth_store from "../stores/auth";
 //--- routes from views
 import home_routes from "../views/Home/routes";
 import login_routes from "../views/Login/routes";
+import usuarios_routes from "../views/Usuarios/routes";
 
 const routes = [
   {
@@ -12,7 +13,7 @@ const routes = [
     name: "about",
     component: () => import("../views/AboutView.vue"),
   },
-].concat(home_routes, login_routes);
+].concat(home_routes, login_routes,usuarios_routes);
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
---
+++ trasys/src/views/Usuários/routes.js
@@ -0,0 +1,11 @@
+import ListUsuariosView from './ListUsuarios.vue';
+
+const routes = [
+  {
+    path: "/usuarios",
+    name: "usuarios",
+    component: ListUsuariosView,
+  }
+];
+
+export default routes;
