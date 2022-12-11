6d7b17bdf1f9c0da770cfd030c79c629349c1984
Ítalo Rodrigo <italo.computation@gmail.com>
2022-12-11 18:36:34 -0400

    Criação de Modal
    Criação de Modal

---
+++ trasys/src/views/Usuarios/components/ModalUsuario.vue
@@ -0,0 +1,19 @@
+<script scoped>
+export default {
+  components:{
+    
+  },
+  name: "ListUsuarios",
+  data() {
+    return {
+      
+    }
+  },
+}
+</script>
+
+<template>
+    <div ref="modal" class="modal-dialog modal-dialog-centered">
+        teste
+    </div>
+</template>
